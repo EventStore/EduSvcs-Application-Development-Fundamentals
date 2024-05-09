12b27ee442dc17726cffe54de7d22278e43bd936
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-05-09 11:59:55 -0500

    Update solution.js

--- labs/Module_02/Lab1/solution.js
+++ labs/Module_02/Lab1/solution.js
@@ -2,28 +2,15 @@ import { EventStoreDBClient,jsonEvent} from "@eventstore/db-client";
 import { FORWARDS,START } from "@eventstore/db-client";
 import { v4 as uuid } from 'uuid'	
 
-const event = jsonEvent({
-  id: uuid(),
-  type: "orderShipped",
-  data: {
-    orderId: "123", // Added orderId to uniquely identify the order
-    customerId: "1",
-    price: 10.00,
-    item:”keyboard”
-  },
-});
-
-await client.appendToStream("orders", event);
-
-const events = client.readStream("orders");
+const events = client.readStream("order-123");
 
 let totalKeyboardSales = 0
     
 for await (const resolvedEvent of events) {
   console.log(resolvedEvent.event?.data);
   let eventData = resolvedEvent.event?.data
-  if (eventData && eventData.item === "keyboard") {
+  if (eventData && resolvedEvent.event?.type === "itemShipped" && (eventData.item === "keyboard") {
     totalKeyboardSales += eventData.price;
   }
 }
-console.log("Total keyboard sales: " + totalKeyboardSales);
+console.log("Total actual keyboard sales: " + totalKeyboardSales);
