4058ecf321026ba7a6b5dce684ce8ba983e0d113
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-06-19 15:23:06 -0500

    Update solution.js

--- labs/Module_01/Lab2/solution.js
+++ labs/Module_01/Lab2/solution.js
@@ -4,20 +4,6 @@ import { v4 as uuid } from 'uuid'
 
 const client = EventStoreDBClient.connectionString("esdb://localhost:2113?tls=false");
 
-
-
-const event = jsonEvent({
-  id: uuid(),
-  type: "orderShipped",
-  data: {
-    customerId: "1",
-    price: 20.00,
-    item:"mouse"
-  },
-});
-
-await client.appendToStream("order-123", event);
-
 const events = client.readStream("order-123", {
   direction: FORWARDS,
   fromRevision: START,
