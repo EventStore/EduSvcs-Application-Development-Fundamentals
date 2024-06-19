b812e90f37094e884d3d7aac6d7a557d35ea9d72
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-06-19 15:05:53 -0500

    Create generate.js

---
+++ labs/00_Validate_Setup/generate.js
@@ -0,0 +1,91 @@
+import { EventStoreDBClient,jsonEvent } from "@eventstore/db-client";
+import { v4 as uuid } from 'uuid'
+
+const client = EventStoreDBClient.connectionString("esdb://localhost:2113?tls=false");
+
+//Add events
+
+//from module 1 demo 1
+const event1 = jsonEvent({
+    id: uuid(),
+    type: "itemAdded",
+    data: {
+      customerId: "1",
+      amount: "50.00",
+      item:"keyboard"
+    },
+});
+      
+const event2 = jsonEvent({
+    id: uuid(),
+    type: "itemAdded",
+    data: {
+      customerId: "1",
+      amount: "20.00",
+      item:"mouse"
+    },
+});
+      
+const event3 = jsonEvent({
+    id: uuid(),
+    type: "orderRequested",
+    data: {
+      customerId: "1",
+      requestor: "John Smith",
+      email:"john@eventstore.com",
+    },
+});
+
+//from module 1 lab 1
+const event4 = jsonEvent({
+    id: uuid(),
+    type: "itemShipped",
+    data: {
+      customerId: "1",
+      amount: "50.00",
+      item:"keyboard"
+    },
+});
+
+//from module 2 demo 2
+const event5 = jsonEvent({
+    id: uuid(),
+    type: "itemShipped",
+    data: {
+      customerId: "1",
+      amount: "20.00",
+      item:"mouse"
+    },
+});
+
+//from module 2 lab 2
+const event6 = jsonEvent({
+    id: uuid(),
+    type: "itemAdded",
+    data: {
+      customerId: "1",
+      amount: "50.00",
+      item:"keyboard"
+    },
+});
+
+const event7 = jsonEvent({
+    id: uuid(),
+    type: "itemShipped",
+    data: {
+      customerId: "1",
+      amount: "50.00",
+      item:"keyboard"
+    },
+});  
+
+await client.appendToStream("order-123", event1);
+await client.appendToStream("order-123", event2);
+await client.appendToStream("order-123", event3);
+await client.appendToStream("order-123", event4);
+await client.appendToStream("order-123", event5);
+await client.appendToStream("order-123", event6);
+await client.appendToStream("order-123", event7);
+
+client.dispose();
+console.log("The events were added to the stream.")
