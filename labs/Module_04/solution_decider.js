93486f3317ee05fc02dc54778267612449a86f6b
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-07-01 22:01:46 -0500

    Create solution_decider.js

---
+++ labs/Module_04/solution_decider.js
@@ -0,0 +1,50 @@
+import { v4 as uuid } from 'uuid'; 
+
+function evolve(state, event) {
+    switch (event.type) {
+      case "inventoryAdded":
+        state.inventory += event.data.count;
+        break;
+      case "inventoryRemoved":
+        state.inventory -= event.data.count;
+        break;
+      case "orderPlaced":
+        state.inventory -= event.data.count;
+        break;
+    }
+    return state;
+  }
+  
+  async function decide(command, state) {
+    if (command.type === "addInventory") {
+      return [{
+        type: "inventoryAdded",
+        data: { count: command.data.count, cost: command.data.cost }
+      }];
+    }
+    if (command.type === "removeInventory") {
+      if (command.data.count <= state.inventory) {
+        return [{
+          type: "inventoryRemoved",
+          data: { count: command.data.count }
+        }];
+      } else {
+        throw new Error('Not enough items in inventory');
+      }
+    }
+    if (command.type === "placeOrder") {
+      if (command.data.count <= state.inventory) {
+        return [
+          {
+            type: "orderPlaced",
+            data: { count: command.data.count, price: command.data.price }
+          }
+        ];
+      } else {
+        throw new Error('Not enough items in inventory to place the order');
+      }
+    }
+    throw new Error(`Unknown command type: ${command.type}`);
+  }
+  
+  export { evolve, decide };
