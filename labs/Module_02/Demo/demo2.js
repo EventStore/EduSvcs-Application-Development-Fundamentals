56c9a98c7a95371a20935e24a2bdbfe51add6cda
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-12-19 12:18:41 -0600

    Update demo2.js

--- labs/Module_02/Demo/demo2.js
+++ labs/Module_02/Demo/demo2.js
@@ -3,22 +3,20 @@ import { EventStoreDBClient,START } from "@eventstore/db-client";
 // Create an EventStoreDB client
 const client = EventStoreDBClient.connectionString("esdb://localhost:2113?tls=false");
 
-let actualSales = 0
+let actualSales = 0;
 
-const subscription = client
- .subscribeToStream("order-123", {
-   fromRevision: START,
- })
- .on("data", (resolvedEvent) => {
-   handleEvent(resolvedEvent);
- });
+const subscription = client.subscribeToStream("order-123",{fromRevision: START,})
+.on("data", (resolvedEvent) => {
+  handleEvent(resolvedEvent);
+});
 
-// Define the event handler function
+console.log("listening for itemShipped events...");
+
 const handleEvent = async (resolvedEvent) => {
- const eventData = resolvedEvent.event?.data;
- if (eventData && resolvedEvent.event?.type === "itemShipped") {
-   console.log("Received event:", eventData);
-   actualSales += parseFloat(eventData.amount);
-   console.log("the actual sales amount is: " + actualSales);
- }
+  const eventData = resolvedEvent.event?.data;
+  if(eventData && resolvedEvent.event?.type === "itemShipped"){
+    console.log("Received event:", eventData);
+    actualSales += parseFloat(eventData.amount);
+    console.log("the actual sales amount is: " +actualSales);
+  }
 };
