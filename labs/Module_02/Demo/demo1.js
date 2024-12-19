bb4870679e0722e9845b7aa1e07990ac6a9e5efc
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-12-19 12:18:08 -0600

    Update demo1.js

--- labs/Module_02/Demo/demo1.js
+++ labs/Module_02/Demo/demo1.js
@@ -3,27 +3,27 @@ import { FORWARDS, START } from "@eventstore/db-client";
 
 const client = EventStoreDBClient.connectionString("esdb://localhost:2113?tls=false");
 
-// In-memory variable to hold the read model data (simple approach)
-let orderSales = { pendingSales: 0, actualSales: 0 };
+let orderSales = {pendingSales:0, actualSales:0};
 
 // **Query: Reading the stream of events for a specific order**
 const stream = await client.readStream("order-123", { direction: FORWARDS, fromRevision: START });
 
   // **Projection: Processing events and updating read model **
 for await (const resolvedEvent of stream) {
-    const event = resolvedEvent.event;
-    const data = event.data;
-    const amount = parseFloat(data.amount || 0);
 
-     if (event.type === "itemAdded") {
-        orderSales.pendingSales += amount;
-     } else if (event.type === "itemShipped") {
-        orderSales.pendingSales -= amount;
-        orderSales.actualSales += amount;
-     }
- }
+   const event = resolvedEvent.event;
+   const data = event.data;
+   const amount = parseFloat(data.amount || 0);
 
-  // Access and potentially format the read model data
+   if(event.type==="itemAdded"){
+      orderSales.pendingSales += amount;
+   }else if(event.type==="itemShipped"){
+      orderSales.pendingSales -= amount;
+      orderSales.actualSales += amount;
+   }
+   
+}
+
 console.log(`Pending sales: $${orderSales.pendingSales}\nActual sales: $${orderSales.actualSales}`);
 
 client.dispose();
