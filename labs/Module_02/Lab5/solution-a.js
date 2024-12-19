0f1d1baa36b5887c87eed92a0b72f6dead6004eb
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-12-19 12:23:35 -0600

    Update solution-a.js

--- labs/Module_02/Lab5/solution-a.js
+++ labs/Module_02/Lab5/solution-a.js
@@ -2,21 +2,15 @@ import { EventStoreDBClient, persistentSubscriptionToStreamSettingsFromDefaults 
 
 const client = EventStoreDBClient.connectionString("esdb://localhost:2113?tls=false");
 
-async function createPersistentSubscription() {
-  const settings = persistentSubscriptionToStreamSettingsFromDefaults({
-    startFrom: 'start',
-    resolveLinkTos: true
-  });
-
-  await client.createPersistentSubscriptionToStream(
-    "inventory",  // The stream name
-    "group1",     // The subscription group name
-    settings
-  );
-
-  console.log("Persistent subscription created");
-}
-
-createPersistentSubscription().catch(error => {
-  console.error("Error creating persistent subscription:", error);
+const settings = persistentSubscriptionToStreamSettingsFromDefaults({
+  startFrom: 'start',
+  resolveLinkTos: true
 });
+
+await client.createPersistentSubscriptionToStream(
+  "order-123",  // The stream name
+  "group1",     // The subscription group name
+  settings
+);
+
+console.log("Persistent subscription created");
