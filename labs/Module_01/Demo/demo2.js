44c47131c2b4dfc0e868a632d03d74be809ac05c
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-12-19 12:16:59 -0600

    Update demo2.js

--- labs/Module_01/Demo/demo2.js
+++ labs/Module_01/Demo/demo2.js
@@ -3,12 +3,13 @@ import { FORWARDS,START } from "@eventstore/db-client";
 
 const client = EventStoreDBClient.connectionString("esdb://localhost:2113?tls=false");
 
-const events = client.readStream("order-123", {
+const events = client.readStream("order-123",{
   direction: FORWARDS,
   fromRevision: START,
 });
 
-for await (const resolvedEvent of events) {
-  console.log(resolvedEvent.event?.data);
+for await (const resolveEvent of events){
+  console.log(resolveEvent.event?.data);
 }
+
 client.dispose();
