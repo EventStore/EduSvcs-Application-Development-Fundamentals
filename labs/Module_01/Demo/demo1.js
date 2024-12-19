d2b8af820f33db3873d53c3957d6097b41c5f4d6
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-12-19 12:16:33 -0600

    Update demo1.js

--- labs/Module_01/Demo/demo1.js
+++ labs/Module_01/Demo/demo1.js
@@ -1,31 +1,31 @@
-import { EventStoreDBClient,jsonEvent } from "@eventstore/db-client";
-import { v4 as uuid } from 'uuid'
+import { EventStoreDBClient, jsonEvent} from "@eventstore/db-client";
+import { v4 as uuid} from 'uuid';
 
 const client = EventStoreDBClient.connectionString("esdb://localhost:2113?tls=false");
 
 const event1 = jsonEvent({
- id: uuid(),
- type: "itemAdded",
- data: {
-   customerId: "1",
-   amount: "50.00",
-   item:"keyboard"
- },
+  id: uuid(),
+  type: "itemAdded",
+  data: {
+    customerId: "1",
+    amount: "50.00",
+    item: "keyboard"
+  },
 });
 
 await client.appendToStream("order-123", event1);
 
 const event2 = jsonEvent({
- id: uuid(),
- type: "itemAdded",
- data: {
-   customerId: "1",
-   amount: "20.00",
-   item:"mouse"
- },
+  id: uuid(),
+  type: "itemAdded",
+  data: {
+    customerId: "1",
+    amount: "20.00",
+    item: "mouse"
+  },
 });
 
 await client.appendToStream("order-123", event2);
 
 client.dispose();
-console.log("The events were added to the stream.")
+console.log("Events were added");
