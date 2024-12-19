b713e43d91d50112e2581c34a624a5fa55242a0d
jeanmarcjulien <julienjeanmarc@gmail.com>
2024-12-19 12:24:52 -0600

    Update solution.js

--- labs/Module_02/Lab6/solution.js
+++ labs/Module_02/Lab6/solution.js
@@ -2,40 +2,41 @@ import { EventStoreDBClient,eventTypeFilter,streamNameFilter} from "@eventstore/
 
 const client = EventStoreDBClient.connectionString("esdb://localhost:2113?tls=false");
 
-
 const subscription1 = client.subscribeToAll({
   	filter: eventTypeFilter({
-  		prefixes: ["item"],
+  		prefixes: ["Invoice"],
 }),
 });
 
 console.log("filtering by event type prefix");
 for await (const resolvedEvent of subscription1) {
+  console.log(resolvedEvent.event?.streamId);
   console.log(resolvedEvent.event?.data);
   console.log(resolvedEvent.event?.type);
 }
 
-
 const subscription2 = client.subscribeToAll({
   	filter: eventTypeFilter({
-  		regex: "^item",
+  		regex: "^Payment",
 }),
 });
 
 console.log("filtering by event type regex");
 for await (const resolvedEvent of subscription2) {
+  console.log(resolvedEvent.event?.streamId);
   console.log(resolvedEvent.event?.data);
   console.log(resolvedEvent.event?.type);
 }
 
 const subscription3 = client.subscribeToAll({
   	filter: streamNameFilter({
-  		prefixes: ["order"],
+  		prefixes: ["invoice"],
 }),
 });
 
 console.log("filtering by stream name prefix");
 for await (const resolvedEvent of subscription3) {
+  console.log(resolvedEvent.event?.streamId);
   console.log(resolvedEvent.event?.data);
   console.log(resolvedEvent.event?.type);
 
@@ -49,6 +50,7 @@ const subscription4 = client.subscribeToAll({
 
 console.log("filtering by stream name regex");
 for await (const resolvedEvent of subscription4) {
+  console.log(resolvedEvent.event?.streamId);
   console.log(resolvedEvent.event?.data);
   console.log(resolvedEvent.event?.type);
 
